5bc07bd274b3988d3f38a8df351e3f3b0f06cc9d
impurshottam <purshottam.indore@gmail.com>
2017-01-08 17:24:23 +0530

    Removing unused code

--- src/app/app.js
+++ src/app/app.js
@@ -3,34 +3,10 @@
     'use strict';
     angular
         .module('app', [])
-        .controller('ParentController',[function(){
+        .controller('ParentController', [function() {
 
         }])
-        .controller('ChildController',[function(){
-        	
+        .controller('ChildController', [function() {
+
         }])
-}(window, angular, undefined));
-;
-// (function(window, angular, undefined) {
-//     'use strict';
-//     angular
-//         .module('app')
-//         .config(config);
-//     config.$inject = ['$stateProvider', '$urlRouterProvider'];
-
-//     function config($stateProvider, $urlRouterProvider) {
-//         $urlRouterProvider.otherwise('/dashboard');
-//     }
-// }(window, angular, undefined));
-// ;
-// (function(window, angular, undefined) {
-//     'use strict';
-//     angular
-//         .module('app')
-//         .run(run);
-//     run.$inject = ['$rootScope'];
-
-//     function run($rootScope) {
-//         $rootScope.title = "Angularjs $log";
-//     }
-// }(window, angular, undefined));
+}(window, angular, undefined));;
